2dcfcb55598029b098d1f41b71663b113d9b80df
Moitzi Franco <fmoitzi@l50.vsc.xcat>
2024-11-20 15:44:35 +0100

    update scripts

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import inspect
+import os
 import subprocess
 import sys
-import psutil
-import os
 
 import joblib
+import psutil
 from joblib import delayed
 
 VERBOSITY = 100000
@@ -22,38 +22,35 @@ except IndexError:
 
 def run_subprocess():
     name = inspect.stack()[0][3]
-    print(f'START: {name}')
-    subprocess.run(['./sample', str(duration)])
-    print(f'END: {name}')
+    print(f"START: {name}")
+    subprocess.run(["./sample", str(duration)], env=os.environ)
+    print(f"END: {name}")
 
 
 def run_parallel_subprocess(number):
     name = inspect.stack()[0][3]
-    print(f'START: {name}')
-    subprocess.run(['mpirun', '-n', str(number), './sample', str(duration)])
-    print(f'END: {name}')
+    print(f"START: {name}")
+    subprocess.run(
+        ["mpirun", "-n", str(number), "./sample", str(duration)], env=os.environ
+    )
+    print(f"END: {name}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     nr_cpu_logical = psutil.cpu_count(logical=True)
-    print(f'CPU (including logical): '
-          f'{nr_cpu_logical:4d}')
+    print(f"CPU (including logical): " f"{nr_cpu_logical:4d}")
 
     nr_cpu_non_logical = psutil.cpu_count(logical=False)
-    print(f'CPU (only physical):     '
-          f'{nr_cpu_non_logical:4d}')
+    print(f"CPU (only physical):     " f"{nr_cpu_non_logical:4d}")
 
     sched_affinity = len(os.sched_getaffinity(0))
-    print(f'CPU (affinity):         '
-          f'{sched_affinity:4d}')
+    print(f"CPU (affinity):          " f"{sched_affinity:4d}")
 
     jobs = jobs // 2
 
-    print(f'Jobs (specified):       '
-          f'{jobs:4d}')
+    print(f"Jobs (specified):        " f"{jobs:4d}")
 
     with joblib.Parallel(n_jobs=jobs, verbose=VERBOSITY) as parallel:
-        parallel_jobs = [delayed(run_parallel_subprocess)(2) for i in range(
-            jobs)]
+        parallel_jobs = [delayed(run_parallel_subprocess)(2) for i in range(jobs)]
 
         parallel(parallel_jobs)
--- serial_run.py
+++ serial_run.py
@@ -1,8 +1,10 @@
 import inspect
+import os
 import subprocess
 import sys
 
 import joblib
+import psutil
 from joblib import delayed
 
 VERBOSITY = 100000
@@ -20,20 +22,33 @@ except IndexError:
 
 def run_subprocess():
     name = inspect.stack()[0][3]
-    print(f'START: {name}')
-    subprocess.run(['./sample', str(duration)])
-    print(f'END: {name}')
+    print(f"START: {name}")
+    subprocess.run(["./sample", str(duration)], env=os.environ)
+    print(f"END: {name}")
 
 
 def run_parallel_subprocess(number):
     name = inspect.stack()[0][3]
-    print(f'START: {name}')
-    subprocess.run(['mpirun', '-n', str(number), './sample', str(duration)])
-    print(f'END: {name}')
+    print(f"START: {name}")
+    subprocess.run(
+        ["mpirun", "-n", str(number), "./sample", str(duration)], env=os.environ
+    )
+    print(f"END: {name}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    nr_cpu_logical = psutil.cpu_count(logical=True)
+    print(f"CPU (including logical): " f"{nr_cpu_logical:4d}")
+
+    nr_cpu_non_logical = psutil.cpu_count(logical=False)
+    print(f"CPU (only physical):     " f"{nr_cpu_non_logical:4d}")
+
+    sched_affinity = len(os.sched_getaffinity(0))
+    print(f"CPU (affinity):          " f"{sched_affinity:4d}")
+
+    print(f"Jobs (specified):        " f"{jobs:4d}")
+
     with joblib.Parallel(n_jobs=jobs, verbose=VERBOSITY) as parallel:
         parallel_jobs = [delayed(run_subprocess)() for i in range(jobs)]
 
-    parallel(parallel_jobs)
+        parallel(parallel_jobs)
